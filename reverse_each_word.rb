17335ef570e5310d201b620806afd8d523e655ed
Kevin Chi <kevinchi118@gmail.com>
2018-06-13 16:08:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
     string = string.split
-    string.map_with_index do |word, index|
+    string.each_with_index.collect do |word, index|
         string[index] = word.reverse
     end
     string.join(" ")
